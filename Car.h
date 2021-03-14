59b5c43b3327a905f1331831520e2d8ae1f38e23
pereiradaniel <danielpaulpereira@protonmail.com>
2021-03-14 13:54:28 -0400

    Add fstream!

--- Car.h
+++ Car.h
@@ -1,14 +1,13 @@
 #ifndef SDDS_CAR_H
 #define SDDS_CAR_H
 #include <iostream>
-
+#include <fstream>
 #include "Vehicle.h" // inherit from Vehicle
 namespace sdds {
 	class Car : public Vehicle {
 		std::string maker;		// name of make
 		std::string condition;	// condition: new, used, or broken.
 		double tSpeed;			// top speed (do not confuse with function topSpeed()!
-	
 	public:
 		Car(std::istream&);						// a custom constructor that receives an object of type `std::istream&` as parameter.
 		std::string condition() const;			// a query that returns `new`, `used` or `broken`, representing the condition of the car
